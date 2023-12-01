a7bb40b93cbfd82e680b556383611eae6b9546b8
Toprak Balci <tbalci@k1m16s06.42istanbul.com.tr>
2023-12-01 19:11:30 +0300

    m

--- main.c
+++ main.c
@@ -6,17 +6,26 @@
 /*   By: tbalci <tbalci@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 21:46:36 by tbalci            #+#    #+#             */
-/*   Updated: 2023/11/28 19:24:41 by tbalci           ###   ########.fr       */
+/*   Updated: 2023/12/01 19:08:09 by tbalci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
 
-// void	mainregulator(t_data *datas, int i)
-// {
-	
-// }
+void	sort(t_data *datas)
+{
+	pb(datas);
+	pb(datas);
+	while (datas->a_l != 0)
+	{
+		mcountinit(datas);
+		regfunc(datas);
+	}
+	b_regulator2(datas);
+	while (datas->b_l != 0)
+		pa(datas);
+}
 
 
 void print_stacks(t_data *datas)
@@ -56,19 +65,5 @@ int main(int ac, char **av)
 	i = 0;
 	datas = malloc(sizeof(t_data));
 	init_stack(av, datas, ac);
-	pb(datas);
-	pb(datas);
-	while (datas->a_l != 0)
-	{
-		mcountinit(datas);
-		regfunc(datas);
-		// mcountprintf(datas);
-		// print_stacks(datas);
-	}
-	b_regulator2(datas);
-	while (datas->b_l != 0)
-	{
-		pa(datas);
-	}
-	// print_stacks(datas);
+	sort(datas);
 }
